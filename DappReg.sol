89e6580b38bdc2ff7249df2d15a4345d9b78ba34
Jaco Greeff <jaco@ethcore.io>
2016-09-16 17:44:50 +0200

    simplify & solidty ^0.4.1

--- DappReg.sol
+++ DappReg.sol
@@ -2,12 +2,14 @@
 //! By Parity Team (Ethcore), 2016.
 //! Released under the Apache Licence 2.
 
+pragma solidity ^0.4.1;
+
 contract Owned {
   event NewOwner(address indexed old, address indexed current);
 
   modifier only_owner {
     if (msg.sender != owner) throw;
-    _
+    _;
   }
 
   address public owner = msg.sender;
@@ -19,19 +21,14 @@ contract Owned {
 }
 
 contract DappReg is Owned {
-  // id       - when we register, we set this from the original repo hash and never change it. The possibility
-  //            exists that we need to have other info in other contracts registering this, so we need a global
-  //            unique value that is the same across all contracts.
-  // repo     - GithubHint for the actual code
+  // id       - shared to be the same accross all contracts for a specific dapp (including GithuHint for the repo)
   // priority - Rating will be dealt with in other contracts, however this just gives us a generic way to move
   //            things up or down if need be. Very high values will probably indicate a default feature, i.e. already
   //            visible without user selection, a 0 means it won't show at all (is deleted)
   // owner    - that guy
-  // meta     - no specific items in mind yet, however could be 'IMG' like in TokenReg, etc. (manifest.json
-  //            could cover a lot of these items, but have the extensibility on-hand)
+  // meta     - meta information for the dapp
   struct Dapp {
     bytes32 id;
-    bytes32 repo;
     uint priority;
     address owner;
     mapping (bytes32 => bytes32) meta;
@@ -39,33 +36,32 @@ contract DappReg is Owned {
 
   modifier when_fee_paid {
     if (msg.value < fee) throw;
-    _
+    _;
   }
 
   modifier only_dapp_owner(bytes32 _id) {
     if (dapps[_id].owner != msg.sender) throw;
-    _
+    _;
   }
 
   modifier both_owner_dapp_owner(bytes32 _id) {
     if (dapps[_id].owner != msg.sender && owner != msg.sender) throw;
-    _
+    _;
   }
 
   modifier when_id_free(bytes32 _id) {
     if (dapps[_id].id != 0) throw;
-    _
+    _;
   }
 
   modifier when_open {
     if (open == 0 && owner != msg.sender) throw;
-    _
+    _;
   }
 
   event MetaChanged(bytes32 indexed id, bytes32 indexed key, bytes32 value);
   event OwnerChanged(bytes32 indexed id, address indexed owner);
   event PriorityChanged(bytes32 indexed id, uint indexed priority);
-  event RepoChanged(bytes32 indexed id, bytes32 repo);
   event Registered(bytes32 indexed id, address indexed owner);
   event Unregistered(bytes32 indexed id);
 
@@ -86,7 +82,6 @@ contract DappReg is Owned {
   function at(uint _idx) constant returns (bytes32 id, bytes32 repo, uint priority, address owner) {
     var d = dapps[ids[_idx]];
     id = d.id;
-    repo = d.repo;
     priority = d.priority;
     owner = d.owner;
   }
@@ -95,28 +90,21 @@ contract DappReg is Owned {
   function get(bytes32 _id) constant returns (bytes32 id, bytes32 repo, uint priority, address owner) {
     var d = dapps[_id];
     id = d.id;
-    repo = d.repo;
     priority = d.priority;
     owner = d.owner;
   }
 
   // add apps
-  function register(bytes32 _repo) when_open when_fee_paid when_id_free(_repo) {
-    dapps[_repo] = Dapp(_repo, _repo, BASE_PRIORITY, msg.sender);
-    ids.push(_repo);
-    Registered(_repo, msg.sender);
+  function register(bytes32 _id) when_open when_fee_paid when_id_free(_id) {
+    ids.push(_id);
+    dapps[_id] = Dapp(_id, BASE_PRIORITY, msg.sender);
+    Registered(_id, msg.sender);
   }
 
   // remove apps
   function unregister(bytes32 _id) both_owner_dapp_owner(_id) {
     dapps[_id].priority = 0;
     Unregistered(_id);
-  }
-
-  // set the actual app repo (GithubHint)
-  function setRepo(bytes32 _id, bytes32 _repo) only_dapp_owner(_id) {
-    dapps[_id].repo = _repo;
-    RepoChanged(_id, _repo);
   }
 
   // get meta information
