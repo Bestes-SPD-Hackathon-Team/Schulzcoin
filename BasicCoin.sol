a64b2dd2a0bb1454feb603813d6c76480fe91102
Jaco Greeff <jaco@ethcore.io>
2016-09-16 23:26:48 +0200

    simplify further - owner gets full balance

--- BasicCoin.sol
+++ BasicCoin.sol
@@ -34,14 +34,12 @@ contract Owned {
   }
 }
 
-contract BasicCoin is Owned, TokenEvents {
+contract BasicCoin is Owned, Token {
   // this is as basic as can be, only the associated balance & allowances
   struct Account {
     uint balance;
     mapping (address => uint) allowanceOf;
   }
-
-  event Buyin(address indexed buyer, uint indexed price, uint indexed amount);
 
   modifier when_owns(address _owner, uint _amount) {
     if (accounts[_owner].balance < _amount) throw;
@@ -53,30 +51,31 @@ contract BasicCoin is Owned, TokenEvents {
     _;
   }
 
-  modifier when_msg_value {
+  modifier when_no_eth {
+    if (msg.value > 0) throw;
+    _;
+  }
+
+  modifier when_msg_has_value {
     if (msg.value == 0) throw;
     _;
   }
 
-  modifier when_nonzero(uint _value) {
+  modifier when_non_zero(uint _value) {
     if (_value == 0) throw;
     _;
   }
 
   // the base, tokens denoted in micros
   uint constant public base = 1000000;
-
   uint public totalSupply;
-  uint public remaining;
-  uint public price;
-
   mapping (address => Account) accounts;
 
   // constructor sets the parameters of execution - price/1m & totalSupply
-  function BasicCoin(uint _price, uint128 _totalSupply) when_nonzero(_price) when_nonzero(_totalSupply) {
+  function BasicCoin(uint128 _totalSupply) when_no_eth when_non_zero(_totalSupply) {
     totalSupply = _totalSupply;
-    remaining = totalSupply;
-    price = _price;
+    accounts[msg.sender].balance = _totalSupply;
+    Transfer(this, msg.sender, _totalSupply);
   }
 
   // balance of a specific address
@@ -85,7 +84,7 @@ contract BasicCoin is Owned, TokenEvents {
   }
 
   // transfer
-  function transfer(address _to, uint _value) when_owns(msg.sender, _value) returns (bool success) {
+  function transfer(address _to, uint _value) when_no_eth when_owns(msg.sender, _value) returns (bool success) {
     Transfer(msg.sender, _to, _value);
     accounts[msg.sender].balance -= _value;
     accounts[_to].balance += _value;
@@ -94,7 +93,7 @@ contract BasicCoin is Owned, TokenEvents {
   }
 
   // transfer via allowance
-  function transferFrom(address _from, address _to, uint256 _value) when_owns(_from, _value) when_has_allowance(_from, msg.sender, _value) returns (bool success) {
+  function transferFrom(address _from, address _to, uint256 _value) when_no_eth when_owns(_from, _value) when_has_allowance(_from, msg.sender, _value) returns (bool success) {
     Transfer(_from, _to, _value);
     accounts[_from].allowanceOf[msg.sender] -= _value;
     accounts[_from].balance -= _value;
@@ -104,7 +103,7 @@ contract BasicCoin is Owned, TokenEvents {
   }
 
   // approve allowances
-  function approve(address _spender, uint256 _value) returns (bool success) {
+  function approve(address _spender, uint256 _value) when_no_eth returns (bool success) {
     Approval(msg.sender, _spender, _value);
     accounts[msg.sender].allowanceOf[_spender] += _value;
 
@@ -116,28 +115,8 @@ contract BasicCoin is Owned, TokenEvents {
     return accounts[_owner].allowanceOf[_spender];
   }
 
-  // buy a number
-  function buyin() when_nonzero(remaining) when_msg_value {
-    var maxSpend = price * remaining / base;
-    var spend = msg.value > maxSpend ? maxSpend : msg.value;
-    var units = spend * base / price;
-
-    Buyin(msg.sender, price, units);
-    remaining -= units;
-    accounts[msg.sender].balance += units;
-
-    // yes, there is an issue here - the very last person will probably overpay for his units
-  }
-
-  // default goes to buyin
+  // no default function, simple contract only, entry-level users
   function() {
-    buyin();
-  }
-
-  // owner can collect his/her funds
-  function drain() only_owner {
-    if (!msg.sender.send(this.balance)) {
-      throw;
-    }
+    throw;
   }
 }
