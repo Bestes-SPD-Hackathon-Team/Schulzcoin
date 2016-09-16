acd01dee50e5d857dfb81195ab5e2fc6803214fe
Jaco Greeff <jaco@ethcore.io>
2016-09-16 23:29:20 +0200

    combine Token & TokenEvents

--- BasicCoin.sol
+++ BasicCoin.sol
@@ -4,12 +4,10 @@
 
 pragma solidity ^0.4.1;
 
-contract TokenEvents {
+contract Token {
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
 
-contract Token is TokenEvents {
   function totalSupply() constant returns (uint256 total);
   function balanceOf(address _owner) constant returns (uint256 balance);
   function transfer(address _to, uint256 _value) returns (bool success);
