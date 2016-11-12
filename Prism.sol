dd3ba6aebe81aab039fa304686a5b6423632ce5e
Gav Wood <gavin@ethcore.io>
2016-11-12 16:00:47 +0100

    Add BasicWallet and Prism.

---
+++ Prism.sol
@@ -0,0 +1,26 @@
+//! Prism ETH/ETC split contract.
+//! By Gav Wood (Ethcore), 2016.
+//! Released under the Apache Licence 2.
+
+contract Prism {
+    address constant theWithdraw = 0xbf4ed7b27f1d666546e30d74d50d173d20bca754;
+    function Prism() {
+        forked = theWithdraw.balance > 1 ether;
+    }
+    
+    function transferETC(address to) {
+        if (forked)
+            throw;
+        if (!to.send(msg.value))
+            throw;
+    }
+
+    function transferETH(address to) {
+        if (!forked)
+            throw;
+        if (!to.send(msg.value))
+            throw;
+    }
+    
+    bool public forked;
+}
