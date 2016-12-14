97f134dac33578c9e3c99d9ade1b054938d21787
Gav Wood <gavin@ethcore.io>
2016-12-14 15:33:28 +0100

    Remove superfluous code.

--- ProofOfEmail.sol
+++ ProofOfEmail.sol
@@ -14,22 +14,6 @@ contract Owned {
 	address public owner = msg.sender;
 }
 
-contract Registry {
-	event DataChanged(bytes32 indexed name, string indexed key, string plainKey);
-
-	function getData(bytes32 _name, string _key) constant returns (bytes32);
-	function getAddress(bytes32 _name, string _key) constant returns (address);
-	function getUint(bytes32 _name, string _key) constant returns (uint);
-}
-
-contract OwnerRegistry {
-	event Reserved(bytes32 indexed name, address indexed owner);
-	event Transferred(bytes32 indexed name, address indexed oldOwner, address indexed newOwner);
-	event Dropped(bytes32 indexed name, address indexed owner);
-
-	function getOwner(bytes32 _name) constant returns (address);
-}
-
 contract ReversibleRegistry {
 	event ReverseConfirmed(string indexed name, address indexed reverse);
 	event ReverseRemoved(string indexed name, address indexed reverse);
