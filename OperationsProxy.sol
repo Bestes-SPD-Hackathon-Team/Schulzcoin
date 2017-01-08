e48bc2c8e65d59fc97080a1901c87afb548e04b9
Gav <gavin@parity.io>
2017-01-08 20:43:16 +0100

    Constructor params in OpPr

--- OperationsProxy.sol
+++ OperationsProxy.sol
@@ -1,5 +1,4 @@
-//pragma solidity ^0.4.7;
-
+pragma solidity ^0.4.7;
 
 contract OperationsFace {
 	function proposeTransaction(bytes32 _txid, address _to, bytes _data, uint _value, uint _gas) returns (uint txSuccess);
@@ -18,20 +17,23 @@ contract OperationsFace {
 	function build(bytes32 _client, bytes32 _checksum) constant returns (bytes32 o_release, bytes32 o_platform);
 	function release(bytes32 _client, bytes32 _release) constant returns (uint32 o_forkBlock, uint8 o_track, uint24 o_semver, bool o_critical);
 	function checksum(bytes32 _client, bytes32 _release, bytes32 _platform) constant returns (bytes32);
-	
+
 	function clientOwner(address _owner) constant returns (bytes32);
 }
 
 /// Specialise proxy wallet. Owner can send transactions unhindered. Delegates
 /// can send only particular transactions to a named Operations contract.
 contract OperationsProxy {
-    function OperationsProxy(address _owner, address _stable, address _beta, address _nightly, address _operations) {
+    function OperationsProxy(address _owner, address _stable, address _beta, address _nightly, address _stableConfirmer, address _betaConfirmer, address _nightlyConfirmer, address _operations) {
         owner = _owner;
         delegate[1] = _stable;
         delegate[2] = _beta;
         delegate[3] = _nightly;
+        confirmer[1] = _stableConfirmer;
+        confirmer[2] = _betaConfirmer;
+        confirmer[3] = _nightlyConfirmer;
         operations = OperationsFace(_operations);
-    }  
+    }
 
 	event Sent(address indexed to, uint value, bytes data);
 	event OwnerChanged(address indexed was, address indexed who);
@@ -51,12 +53,12 @@ contract OperationsProxy {
 		if (!_to.call.value(_value)(_data)) throw;
 		Sent(_to, _value, _data);
 	}
-	
+
 	function setOwner(address _owner) only_owner {
 	    OwnerChanged(owner, _owner);
 	    owner = _owner;
 	}
-	
+
 	function setDelegate(address _delegate, uint8 _track) only_owner {
 	    DelegateChanged(delegate[_track], _delegate, _track);
 	    delegate[_track] = _delegate;
@@ -70,18 +72,18 @@ contract OperationsProxy {
 	function addRelease(bytes32 _release, uint32 _forkBlock, uint8 _track, uint24 _semver, bool _critical) {
 	    if (relayOrConfirm(_track))
 	        AddReleaseRelayed(_track, _release);
-	    else 
+	    else
 	        trackOfPendingRelease[_release] = _track;
 	}
-	
+
 	function addChecksum(bytes32 _release, bytes32 _platform, bytes32 _checksum) {
 	    var track = trackOfPendingRelease[_release];
 	    if (track == 0)
 	        track = operations.track(operations.clientOwner(this), _release);
 	    if (relayOrConfirm(track))
 	        AddChecksumRelayed(_release, _platform);
-	}	    
-	
+	}
+
 	function relayOrConfirm(uint8 _track) internal only_delegate_of_track(_track) returns (bool) {
 	    if (confirmer[_track] != 0) {
 	        var h = sha3(msg.data);
@@ -94,26 +96,26 @@ contract OperationsProxy {
 	        return true;
 	    }
 	}
-	
-	function confirm(uint8 _track, bytes32 _hash) only_confirmer_of_track(_track) {
+
+	function confirm(uint8 _track, bytes32 _hash) only_confirmer_of_track(_track) payable {
 	    if (!address(operations).call.value(msg.value)(waiting[_track][_hash])) throw;
 	    delete waiting[_track][_hash];
 	    RequestConfirmed(_track, _hash);
 	}
-	
+
 	function reject(uint8 _track, bytes32 _hash) only_confirmer_of_track(_track) {
 	    delete waiting[_track][_hash];
 	    RequestRejected(_track, _hash);
 	}
-	
+
 	function relay() internal {
 	    if (!address(operations).call.value(msg.value)(msg.data)) throw;
 	}
-	
+
 	function cleanupRelease(bytes32 _release) only_confirmer_of_track(trackOfPendingRelease[_release]) {
 	    delete trackOfPendingRelease[_release];
 	}
-	
+
 	function kill() only_owner {
 	    suicide(msg.sender);
 	}
@@ -121,7 +123,7 @@ contract OperationsProxy {
 	modifier only_owner { if (msg.sender != owner) throw; _; }
 	modifier only_delegate_of_track(uint8 track) { if (delegate[track] != msg.sender) throw; _; }
 	modifier only_confirmer_of_track(uint8 track) { if (confirmer[track] != msg.sender) throw; _; }
-	
+
     address public owner;
     mapping(uint8 => address) public delegate;
     mapping(uint8 => address) public confirmer;
