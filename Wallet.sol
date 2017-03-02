095252693ef01e54f52e5fba668dfced445efdd2
Gav Wood <gavin@parity.io>
2017-03-02 13:46:22 +0100

    Grumble.

--- Wallet.sol
+++ Wallet.sol
@@ -372,7 +372,7 @@ contract Wallet is multisig, multiowned, daylimit {
 
     // confirm a transaction through just the hash. we use the previous transactions map, m_txs, in order
     // to determine the body of the transaction from the hash provided.
-    function confirm(bytes32 _h) onlymanyowners(_h) returns (bool) {
+    function confirm(bytes32 _h) onlymanyowners(_h) returns (bool o_success) {
         if (m_txs[_h].to != 0 || m_txs[_h].value != 0 || m_txs[_h].data.length != 0) {
 			address created;
 			if (m_txs[_h].to == 0) {
